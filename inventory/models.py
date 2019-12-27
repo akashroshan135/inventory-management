3449490459d17672bae78a15faef53f64b4688e5
Akash Roshan <akash.second1999@gmail.com>
2019-12-27 17:06:04 +0530

    added models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+#contains the category
+class Category(models.Model):
+    category = models.CharField(max_lenght=50)
+
+    def __str__(self):
+        return self.name
+    
+#contains the details of the stock
+class Stock(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+	    return self.name
+
+#contains the purchases made
+class Purchases(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=100)
+    time = models.DateTimeField(auto_now=True)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+	    return self.name
