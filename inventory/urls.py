b4769b1f40264474da267f010f9b8f28310e0088
Akash Roshan <akash.second1999@gmail.com>
2020-01-15 17:17:19 +0530

    added function to delete items from db

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home'),                                              # the 'HomeView' class in the views.py is rendered as view is called when localhost is called
     path('inventory/', views.InventoryListView.as_view(), name = 'inventory'),                      # the 'InventoryListView' class in the views.py is rendered as view is called when localhost is called
-    path('inventory/new-stock', views.StockCreateView.as_view(), name = 'new-stock'),               # the 'StockCreateView' class in the views.py is rendered as view is called when localhost is called
-    path('inventory/<pk>/edit-stock', views.StockUpdateView.as_view(), name = 'edit-stock'),        # the 'StockUpdateView' class in the views.py is rendered as view is called when localhost is called
+    path('inventory/new', views.StockCreateView.as_view(), name = 'new-stock'),                     # the 'StockCreateView' class in the views.py is rendered as view is called when localhost is called
+    path('inventory/<pk>/edit', views.StockUpdateView.as_view(), name = 'edit-stock'),              # the 'StockUpdateView' class in the views.py is rendered as view is called when localhost is called
+    path('inventory/<pk>/delete', views.StockDeleteView.as_view(), name = 'delete-stock'),          # the 'StockDeleteView' class in the views.py is rendered as view is called when localhost is called
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,51 +1,62 @@
-from django.shortcuts import render                                         # 'render' function used to render a html page
+from django.shortcuts import render                                                     # 'render' function used to render a html page
 from django.views.generic import (
     TemplateView, 
     ListView,
     CreateView, 
     UpdateView,
     DeleteView
-)                                                                           # various views for class based views
-from .models import Stock                                                   # imports models
-from .forms import StockForm                                                # imports forms
-from django.contrib.messages.views import SuccessMessageMixin               # mixin used to display success message
+)                                                                                       # various views for class based views
+from .models import Stock                                                               # imports models
+from .forms import StockForm                                                            # imports forms
+from django.contrib.messages.views import SuccessMessageMixin                           # mixin used to display success message
+from django.contrib import messages                                                     # used for messages 
 
 
-
-class HomeView(TemplateView):                                               # class to render home page
-    template_name = "home.html"                                             # 'home.html' used as the template
-
-
-class InventoryListView(ListView):                                          # class to render inventory page
-    model = Stock                                                           # setting 'Stock' model as model
-    context_object_name = 'stock_items'                                     # 'Stock' model set as 'stock_items' context
-    template_name = "inventory.html"                                        # 'inventory.html' used as the template
+class HomeView(TemplateView):                                                           # class to render home page
+    template_name = "home.html"                                                         # 'home.html' used as the template
 
 
-class StockCreateView(SuccessMessageMixin, CreateView):                     # createview class to render new stock page, mixin used to display message
-    model = Stock                                                           # setting 'Stock' model as model
-    form_class = StockForm                                                  # setting 'StockForm' form as form
-    template_name = "edit_stock.html"                                       # 'edit_stock.html' used as the template
-    success_url = '/inventory'                                              # redirects to 'inventory' page in the url after submitting the form
-    success_message = "Stock has been created successfully"                 # displays message when form is submitted
+class InventoryListView(ListView):                                                      # class to render inventory page
+    model = Stock                                                                       # setting 'Stock' model as model
+    context_object_name = 'stock_items'                                                 # 'Stock' model set as 'stock_items' context
+    template_name = "inventory.html"                                                    # 'inventory.html' used as the template
 
-    def get_context_data(self, **kwargs):                                   # used to send additional context
+
+class StockCreateView(SuccessMessageMixin, CreateView):                                 # createview class to add new stock, mixin used to display message
+    model = Stock                                                                       # setting 'Stock' model as model
+    form_class = StockForm                                                              # setting 'StockForm' form as form
+    template_name = "edit_stock.html"                                                   # 'edit_stock.html' used as the template
+    success_url = '/inventory'                                                          # redirects to 'inventory' page in the url after submitting the form
+    success_message = "Stock has been created successfully"                             # displays message when form is submitted
+
+    def get_context_data(self, **kwargs):                                               # used to send additional context
         context = super().get_context_data(**kwargs)
         context["title"] = 'New Stock'
         context["savebtn"] = 'Add to Inventory'
         return context       
 
 
-class StockUpdateView(SuccessMessageMixin, UpdateView):                     # update view class to render new stock page, mixin used to display message
-    model = Stock                                                           # setting 'Stock' model as model
-    form_class = StockForm                                                  # setting 'StockForm' form as form
-    template_name = "edit_stock.html"                                       # 'edit_stock.html' used as the template
-    success_url = '/inventory'                                              # redirects to 'inventory' page in the url after submitting the form
-    success_message = "Stock has been updated successfully"                 # displays message when form is submitted
+class StockUpdateView(SuccessMessageMixin, UpdateView):                                 # updateview class to edit stock, mixin used to display message
+    model = Stock                                                                       # setting 'Stock' model as model
+    form_class = StockForm                                                              # setting 'StockForm' form as form
+    template_name = "edit_stock.html"                                                   # 'edit_stock.html' used as the template
+    success_url = '/inventory'                                                          # redirects to 'inventory' page in the url after submitting the form
+    success_message = "Stock has been updated successfully"                             # displays message when form is submitted
 
-    def get_context_data(self, **kwargs):                                   # used to send additional context
+    def get_context_data(self, **kwargs):                                               # used to send additional context
         context = super().get_context_data(**kwargs)
         context["title"] = 'Edit Stock'
         context["savebtn"] = 'Update Stock'
         context["delbtn"] = 'Delete Stock'
         return context
+
+
+class StockDeleteView(DeleteView):                                                      # deleteview class to delete stock
+    model = Stock                                                                       # setting 'Stock' model as model
+    template_name = "delete_stock.html"                                                 # 'delete_stock.html' used as the template
+    success_url = '/inventory'                                                          # redirects to 'inventory' page in the url after submitting the form
+    success_message = "Stock has been deleted successfully"                             # displays message when form is submitted
+
+    def delete(self, request, *args, **kwargs):                                         #used to show success message after object is deleted
+        messages.success(self.request, self.success_message)
+        return super(StockDeleteView, self).delete(request, *args, **kwargs)
