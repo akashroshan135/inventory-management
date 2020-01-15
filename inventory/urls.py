4abd88e69d697eb8f95597373dca86c6ae5f8100
Akash Roshan <akash.second1999@gmail.com>
2020-01-15 16:00:55 +0530

    added option to create and edit stock

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home'),                                              # the 'HomeView' class in the views.py is rendered as view is called when localhost is called
     path('inventory/', views.InventoryListView.as_view(), name = 'inventory'),                      # the 'InventoryListView' class in the views.py is rendered as view is called when localhost is called
     path('inventory/new-stock', views.StockCreateView.as_view(), name = 'new-stock'),               # the 'StockCreateView' class in the views.py is rendered as view is called when localhost is called
+    path('inventory/<pk>/edit-stock', views.StockUpdateView.as_view(), name = 'edit-stock'),        # the 'StockUpdateView' class in the views.py is rendered as view is called when localhost is called
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,48 +1,51 @@
 from django.shortcuts import render                                         # 'render' function used to render a html page
 from django.views.generic import (
     TemplateView, 
+    ListView,
     CreateView, 
+    UpdateView,
     DeleteView
 )                                                                           # various views for class based views
 from .models import Stock                                                   # imports models
 from .forms import StockForm                                                # imports forms
-from django.contrib import messages                                         # imports messages fuctions
+from django.contrib.messages.views import SuccessMessageMixin               # mixin used to display success message
+
 
 
 class HomeView(TemplateView):                                               # class to render home page
     template_name = "home.html"                                             # 'home.html' used as the template
 
-class InventoryListView(TemplateView):                                      # class to render inventory page
-    #TODO: create a list view to display stock items
+
+class InventoryListView(ListView):                                          # class to render inventory page
+    model = Stock                                                           # setting 'Stock' model as model
+    context_object_name = 'stock_items'                                     # 'Stock' model set as 'stock_items' context
     template_name = "inventory.html"                                        # 'inventory.html' used as the template
 
-class StockCreateView(CreateView):                                          # createview class to render new stock page
+
+class StockCreateView(SuccessMessageMixin, CreateView):                     # createview class to render new stock page, mixin used to display message
     model = Stock                                                           # setting 'Stock' model as model
     form_class = StockForm                                                  # setting 'StockForm' form as form
-    template_name = "new_stock.html"                                        # 'new_stock.html' used as the template
-    #BUG: success url doesn't work
-    success_url = 'inventory'                                               # redirects to 'inventory' page in the url after submitting the form
+    template_name = "edit_stock.html"                                       # 'edit_stock.html' used as the template
+    success_url = '/inventory'                                              # redirects to 'inventory' page in the url after submitting the form
+    success_message = "Stock has been created successfully"                 # displays message when form is submitted
 
-"""
-def new_stock(request):                                                     # new stock page function
-    
-    stockform = NewStockForm(request.POST)                                  # stores the new stock form in 'stockform' variable
-    
-    if request.method == 'POST':                                            # if block is executed when the POST request is recieved. Used to verify that data is recieved to be stored
-        
-        if stockform.is_valid():                                            # if block is executed only when the form is valid (all mandatory fields are filled)
-            
-            stock = stockform.save(commit=False)                            # saves the 'stockform' in a variable
-            #thread.created_user = request.user                             # set 'created_user' field to current user logged in
-            #thread.forum = forum                                           # set 'forum' field to current forum
-            stock.save()                                                    # saves the form in the database
-                        
-            return redirect('inventory')                                    # redirects to 'inventory' page
-        
-        else:
-        
-            stockform = NewStockForm(request.POST)                          # stores the new stock form in 'stockform' variable. Resets the form
-            messages.warning(request, f"Please fill all the fields")        # shows message when form is invalid
-    
-    return render(request, 'new_stock.html')                                # request to render the 'new_stock.html'
-"""
+    def get_context_data(self, **kwargs):                                   # used to send additional context
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'New Stock'
+        context["savebtn"] = 'Add to Inventory'
+        return context       
+
+
+class StockUpdateView(SuccessMessageMixin, UpdateView):                     # update view class to render new stock page, mixin used to display message
+    model = Stock                                                           # setting 'Stock' model as model
+    form_class = StockForm                                                  # setting 'StockForm' form as form
+    template_name = "edit_stock.html"                                       # 'edit_stock.html' used as the template
+    success_url = '/inventory'                                              # redirects to 'inventory' page in the url after submitting the form
+    success_message = "Stock has been updated successfully"                 # displays message when form is submitted
+
+    def get_context_data(self, **kwargs):                                   # used to send additional context
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Edit Stock'
+        context["savebtn"] = 'Update Stock'
+        context["delbtn"] = 'Delete Stock'
+        return context
