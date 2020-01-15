b303288fccce331918988f8fdf7ed538b8eaaf9f
Akash Roshan <akash.second1999@gmail.com>
2020-01-15 11:34:44 +0530

    modified models and couple of template changes

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Stock, Category, Purchases
 
-# Register your models here.
+#class ForumAdmin(admin.ModelAdmin):
+#    list_display = ('name', 'forum_type', 'forum')
+#    list_filter = ('forum_type', 'issubforum')
+
+admin.site.register(Stock)                                  # shows the Stock that are created in the admin page
+admin.site.register(Purchases)                              # shows the Purchases that are created in the admin page
--- inventory/models.py
+++ inventory/models.py
@@ -1,19 +1,18 @@
 from django.db import models
-
-#contains the category
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
     
 #contains the details of the stock
 class Stock(models.Model):
     id = models.AutoField(primary_key=True)
-    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    type = (
+        ('t0', 'test0'),
+        ('t1', 'test1'),
+        ('t2', 'test2'),
+        ('t3', 'test3'),
+    )
+    stock_type = models.CharField(max_length=2, choices=type)
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
-    price = models.IntegerField()
+    price = models.IntegerField(default=0)
     quantity = models.IntegerField(default=0)
 
     def __str__(self):
