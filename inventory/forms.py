0ec8b507bb99d21476fe4481cac08638aea3b34c
Akash Roshan <akash.second1999@gmail.com>
2020-02-15 10:15:05 +0530

    change stock model and ui changes

--- inventory/forms.py
+++ inventory/forms.py
@@ -5,13 +5,11 @@ class StockForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):                                                        # used to set css classes to the various fields
         super().__init__(*args, **kwargs)
         self.fields['name'].widget.attrs.update({'class': 'textinput form-control'})
-        self.fields['stock_type'].widget.attrs.update({'class': 'textinput form-control'})
         self.fields['quantity'].widget.attrs.update({'class': 'textinput form-control', 'min': '0'})
-        self.fields['price'].widget.attrs.update({'class': 'textinput form-control', 'min': '0'})
 
     class Meta:
         model = Stock
-        fields = ['name', 'stock_type', 'description', 'quantity', 'price']
+        fields = ['name', 'description', 'quantity']
         widgets = {
             'description' : forms.Textarea(
                 attrs = {
--- inventory/models.py
+++ inventory/models.py
@@ -3,17 +3,9 @@ from django.db import models
 #contains the details of the stock FIXME: fix fields
 class Stock(models.Model):
     id = models.AutoField(primary_key=True)
-    type = (
-        ('t0', 'test0'),
-        ('t1', 'test1'),
-        ('t2', 'test2'),
-        ('t3', 'test3'),
-    )
-    stock_type = models.CharField(max_length=2, choices=type)
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=255)
     quantity = models.IntegerField(default=1)
-    price = models.IntegerField(default=1)
 
     def __str__(self):
 	    return self.name
