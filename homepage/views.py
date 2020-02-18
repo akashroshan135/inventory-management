d1aa98a608bb89ef10dfc9ea5143a2f9bcb57ac6
Akash Roshan <akash.second1999@gmail.com>
2020-02-18 12:03:36 +0530

    update homepage, add bar graph

--- homepage/views.py
+++ homepage/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View, TemplateView
+from inventory.models import Stock
+from transactions.models import SaleBill, PurchaseBill
 
 
-# TODO: add charts
-class HomeView(TemplateView):
+class HomeView(View):
     template_name = "home.html"
-
+    def get(self, request):        
+        labels = []
+        data = []        
+        stockqueryset = Stock.objects.order_by('-quantity')
+        for item in stockqueryset:
+            labels.append(item.name)
+            data.append(item.quantity)
+        context = {
+            'labels'    : labels,
+            'data'      : data,
+        }
+        return render(request, 'home.html', context)
 
 class AboutView(TemplateView):
     template_name = "about.html"
